7aad90de1a4d71ca3ed5770cbbc5758aee07718c
tjdgus <tjdgus7562@naver.com>
2022-04-02 15:26:59 +0900

    1. 이니셜 프로젝트

---
+++ README.md
@@ -0,0 +1,11 @@
+# 네이버 지도 API 사용 테스트
+
+### 기술 스택
+- devtools
+- spring web
+- lombok
+- h2
+- mustache
+- jpa
+- springboot 2. 5. 12
+- gradle
---
+++ src/main/java/site/metacoding/mapapitest/MapapitestApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.mapapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapapitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapapitestApplication.class, args);
+	}
+
+}
